ac169ee8daed5819ef375f1980eef161a386b735
Aryan Mittal <aaryanm2005@gmail.com>
2021-06-12 19:21:08 -0400

    changed all charfields to textfields

--- balanced_news/news/models.py
+++ balanced_news/news/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Headline(models.Model):
-    leaning = models.CharField(max_length=200)
+    leaning = models.TextField()
     title = models.TextField()
     img = models.URLField(null=True, blank=True)
     mins_ago = models.IntegerField(default=1440)
-    time_ago_str = models.CharField(max_length=200, null=True)
+    time_ago_str = models.TextField(null=True)
     url = models.URLField()
 
     def __str__(self):
