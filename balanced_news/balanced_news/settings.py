05ef1e45824dce7dd92b5ea6c77a532944e03d34
Aryan Mittal <aaryanm2005@gmail.com>
2021-06-12 16:46:57 -0400

    temporarily stored raw secret key in settings.py

--- balanced_news/balanced_news/settings.py
+++ balanced_news/balanced_news/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'x67v2)^6$xtxfud9j2j#ik1cm=#=%^)p$=0ft0a5&(g+mwzr&y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
