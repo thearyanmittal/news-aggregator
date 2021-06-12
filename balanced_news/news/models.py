9a53e0d5dad104ed8f0b22c185202c44b7558dc1
Aryan Mittal <aaryanm2005@gmail.com>
2021-06-12 19:13:45 -0400

    changed headline titles to textfield

--- balanced_news/news/models.py
+++ balanced_news/news/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Headline(models.Model):
     leaning = models.CharField(max_length=200)
-    title = models.CharField(max_length=200)
+    title = models.TextField()
     img = models.URLField(null=True, blank=True)
     mins_ago = models.IntegerField(default=1440)
     time_ago_str = models.CharField(max_length=200, null=True)
