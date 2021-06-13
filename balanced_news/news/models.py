422109664152d75da2390f7535eabade3d221890
Aryan Mittal <aaryanm2005@gmail.com>
2021-06-12 20:16:43 -0400

    increased max_length for urlfields to 1000

--- balanced_news/news/models.py
+++ balanced_news/news/models.py
@@ -4,10 +4,10 @@ from django.db import models
 class Headline(models.Model):
     leaning = models.TextField()
     title = models.TextField()
-    img = models.URLField(null=True, blank=True)
+    img = models.URLField(max_length=1000, null=True, blank=True)
     mins_ago = models.IntegerField(default=1440)
     time_ago_str = models.TextField(null=True)
-    url = models.URLField()
+    url = models.URLField(max_length=1000)
 
     def __str__(self):
         return str(self.title)
