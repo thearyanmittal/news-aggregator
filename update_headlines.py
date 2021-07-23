79ec8ea13d72662cd87248889e9d2e1a67a54001
Aryan Mittal <aaryanm2005@gmail.com>
2021-07-23 13:34:18 -0400

    re-added scheduler to update headlines

--- update_headlines.py
+++ update_headlines.py
@@ -1,10 +1,12 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from balanced_news.news.getnews import getnews
+from balanced_news.news.models import Headline
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', hour='13', minute='10')
+@sched.scheduled_job('cron', hour='13', minute='40')
 def update_headlines():
+    Headline.objects.all().delete()
     getnews()
 
 sched.start()
