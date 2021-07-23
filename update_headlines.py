6d0631fd65bfc498c9b87c741dfda40482386669
Aryan Mittal <aaryanm2005@gmail.com>
2021-07-23 13:07:28 -0400

    changed cron scheduler time for testing

--- update_headlines.py
+++ update_headlines.py
@@ -3,7 +3,7 @@ from balanced_news.news.getnews import getnews
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', hour='13', minute='6')
+@sched.scheduled_job('cron', hour='13', minute='10')
 def update_headlines():
     getnews()
 
