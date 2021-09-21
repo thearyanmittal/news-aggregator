f70eb5b7cc6f5798ff9021fbc0eb1bc19c334944
Aryan Mittal <aaryanm2005@gmail.com>
2021-09-21 17:22:07 -0400

    added publish time for NPR articles

--- balanced_news/news/getnews/news_sites/getnpr.py
+++ balanced_news/news/getnews/news_sites/getnpr.py
@@ -1,6 +1,8 @@
 from ...models import Headline
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
+from dateparser import parse
 
 def getnpr(per_site):
     html = requests.get('https://www.npr.org/sections/news/').text
@@ -16,7 +18,11 @@ def getnpr(per_site):
             headline.title = art.find('h2').text
             headline.img = art.find('img')['src']
             headline.url = art.find('a')['href']
-            headline.time_ago_str = 'recently'
+
+            time_soup = BeautifulSoup(requests.get(headline.url).text, 'lxml')
+            pubdt = parse(time_soup.find('span', class_='time').text[:-3])
+            time_ago = (datetime.now() - pubdt).total_seconds()
+            headline.mins_ago = int(time_ago // 60)
 
             headline.save()
             count += 1
